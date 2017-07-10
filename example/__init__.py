70a837fdedb78843ad2a4f8f0d03f291af7ab30e
Tim Savage <tim.savage@westpacdatabank.com.au>
2017-07-10 19:06:30 +1000

    Updated example with swagger

--- example/__init__.py
+++ example/__init__.py
@@ -1,8 +1,9 @@
 import odin
 
 from bottle import Bottle
-from odinweb.api import ResourceApi, ApiVersion, detail, listing
+from odinweb import api
 from odinweb.bottle import Api
+from odinweb.swagger import SwaggerSpec
 
 app = Bottle()
 
@@ -12,25 +13,40 @@ class User(odin.Resource):
     name = odin.StringField()
 
 
-class UserApi(ResourceApi):
+class UserApi(api.ResourceApi):
     resource = User
 
-    @listing
+    @api.listing
     def get_user_list(self, request, limit, offset):
         return [
             User(1, "tim"),
             User(2, "sara"),
         ], 2
 
-    @detail
+    @api.create
+    def create_user(self, request):
+        pass
+
+    @api.detail
+    @api.operation_doc(tags=['user'])
+    @api.parameter_doc('full', api.IN_QUERY, type_=api.TYPE_BOOLEAN)
+    @api.response_doc(200, 'Return requested user.')
     def get_user(self, request, resource_id):
+        """
+        Get a user object
+        """
         return User(resource_id, "tim")
+
+    @api.update
+    def update_user(self, request, resource_id):
+        pass
 
 
 app.merge(
     Api(
-        ApiVersion(
-            UserApi()
+        api.ApiVersion(
+            SwaggerSpec(title="Bottle API Swagger"),
+            UserApi(),
         ),
         debug_enabled=True
     )
