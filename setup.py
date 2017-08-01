4767c63d0bb06e47e001bb208269ce0421fb3dc1
Tim Savage <tim@savage.company>
2017-08-02 00:31:59 +1000

    Bumped version

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ with open(path.join(here, 'README.rst'), encoding='utf-8') as f:
 setup(
     name='odinweb.bottle',
     namespace_packages=['odinweb'],
-    version='0.1',
+    version='0.2',
     description="Toolkit for building web API's using Odin and Bottle.",
     long_description=long_description,
     url='https://github.com/python-odin/odinweb.bottle',
